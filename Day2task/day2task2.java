07d40427268ca3137dd54d18e9631872b75d9454
KGCAS STUDENTS <casstudent@kgisledu.com>
2025-06-09 10:47:15 +0530

    tasks completed

---
+++ Day2task/day2task2.java
@@ -0,0 +1,36 @@
+package addon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class day2task2 {
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+	
+		System.out.println("Enter the size of an array:");
+		int n=sc.nextInt();
+		int num[]=new int[n];
+		for(int i=0;i<num.length;i++) {
+				num[i]=sc.nextInt();}
+				System.out.println(Arrays.toString(num));
+			int num1[]=new int[n];
+		
+		for(int i=0;i<num.length;i++) {
+			num1[i]=num[i];
+			
+		}
+		for(int i=0;i<num1.length;i++) {
+			for(int j=0;j<num1.length;j++) {
+				if(num1[i]>num1[j]) {
+					int temp=num1[i];
+					num1[i]=num1[j];
+					num1[j]=temp;}
+			}
+		}
+			
+		 System.out.println(Arrays.toString(num1));
+	
+
+
+	}
+}
---
+++ Day2task/day2task3.java
@@ -0,0 +1,30 @@
+package addon;
+
+public class day2task3 {
+	
+	    public static boolean isPalindrome(String str) {
+	    	str = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+	    	int left = 0;
+	        int right = str.length() - 1;
+	        while (left < right) {
+	            if (str.charAt(left) != str.charAt(right))
+	            {
+	                return false; 
+	            }
+	              left++;
+	              right--;
+	        }
+                return true; 
+	    }
+	    public static void main(String[] args) {
+	        String input = "Sir";
+	        if (isPalindrome(input)) {
+	            System.out.println("Palindrome");
+	        } else {
+	            System.out.println("Not a palindrome");
+	        }
+	    }
+	}
+
+
+
