2888926bbcb9100c8647a519f69b558145cec0f3
JoshikaGowrishankar <joshikagowrishankar13706@gmail.com>
2025-06-09 12:55:50 +0530

    Update task2.java

--- Day1 tasks/task2.java
+++ Day1 tasks/task2.java
@@ -2,25 +2,22 @@ package addon;
 import java.util.Scanner;
 
 public class task2 {
-	
 	public static void main(String[] args) {
-			        int[] numbers = {4, 7, 1, 9, -2, 5};
-
-			        int min = numbers[0];
-			        int max = numbers[0];
-
-			        for (int i = 1; i < numbers.length; i++) {
-			            if (numbers[i] < min) {
-			                min = numbers[i];
-			            }
-			            if (numbers[i] > max) {
-			                max = numbers[i];
-			            }
-			        }
-
-			        System.out.println("Minimum value: " + min);
-			        System.out.println("Maximum value: " + max);
-			    }
+		int[] numbers = {4, 7, 1, 9, -2, 5};
+		int min = numbers[0];
+		int max = numbers[0];
+		for (int i = 1; i < numbers.length; i++) {
+			if (numbers[i] < min) 
+			{
+				min = numbers[i];
 			}
+			if (numbers[i] > max) {
+				max = numbers[i];
+			}
+		}
+		System.out.println("Minimum value: " + min);
+		System.out.println("Maximum value: " + max);
+	}
+}
 
 
