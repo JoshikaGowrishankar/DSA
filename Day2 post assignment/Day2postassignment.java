9831f573801e2b637d48f3256949d17fb1dccc35
JoshikaGowrishankar <joshikagowrishankar13706@gmail.com>
2025-06-09 12:49:09 +0530

    Update Day2postassignment.java

--- Day2 post assignment/Day2postassignment.java
+++ Day2 post assignment/Day2postassignment.java
@@ -1,37 +1,35 @@
 package addon;
 
 public class Day2postassignment {
-	
-	    public static int[] findPairWithTargetSum(int[] arr, int target) {
+	public static int[] findPairWithTargetSum(int[] arr, int target) {
 	        int left = 0;
 	        int right = arr.length - 1;
-
-	        while (left < right) {
-	            int currentSum = arr[left] + arr[right];
-
-	            if (currentSum == target) {
+		while (left < right) {
+			int currentSum = arr[left] + arr[right];
+			if (currentSum == target) 
+			{
 	                return new int[]{arr[left], arr[right]};
 	            } else if (currentSum < target) {
 	                left++;  
-	            } else {
+			} else
+			{
 	                right--; 
-	            }
+			}
 	        }
-
-	        return new int[]{}; }
-
-	    public static void main(String[] args) {
+		return new int[]{};
+	}
+	public static void main(String[] args) {
 	        int[] arr = {1, 2, 3, 4, 6};
 	        int target = 6;
 	        int[] result = findPairWithTargetSum(arr, target);
-
-	        if (result.length == 2) {
+		if (result.length == 2) {
 	            System.out.println("Pair found: " + result[0] + " and " + result[1]);
-	        } else {
+	        } else
+		{
 	            System.out.println("No pair found.");
 	        }
-	    }
 	}
+}
 
 
 
