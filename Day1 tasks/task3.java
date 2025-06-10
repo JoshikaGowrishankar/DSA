3bb8974adffa1765679d5215b753c15189dcf22b
KGCAS STUDENTS <casstudent@kgisledu.com>
2025-06-10 09:48:26 +0530

    taskscompleted

---
+++ Day1 tasks/task3.java
@@ -0,0 +1,57 @@
+package addon;
+import java.util.Scanner;
+
+
+public class task3 {
+	
+
+	
+
+		   static long factorial(int n) {
+
+		        if (n == 0 || n == 1) {
+
+		            return 1; 
+
+		        } else {
+
+		            return n * factorial(n - 1);
+
+		        }
+
+		    }
+
+
+
+		    public static void main(String[] args) {
+
+		        Scanner scanner = new Scanner(System.in);
+
+
+
+		       
+
+		        System.out.print("Enter a number to find its factorial: ");
+
+		        int number = scanner.nextInt();
+
+		        if (number < 0) {
+
+		            System.out.println("Factorial is not defined for negative numbers.");
+
+		        } else {
+
+		            long result = factorial(number);
+
+		            System.out.println("Factorial of " + number + " is: " + result);
+
+		        }
+
+
+
+		        scanner.close();
+
+		    }
+
+		}
+
