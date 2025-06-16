c5f47983538226ed530be1acbeb776453b4360a8
KGCAS STUDENTS <casstudent@kgisledu.com>
2025-06-16 11:59:46 +0530

    tasks completed

---
+++ Day7tasks/Day7task4.java
@@ -0,0 +1,32 @@
+package addon;
+
+public class Day7task4 {
+	
+	
+	    public static void selectionSort(int[] arr) {
+	        int n = arr.length;
+
+	         for (int i = 0; i < n - 1; i++) {
+	          
+	            int minIndex = i;
+
+	            for (int j = i + 1; j < n; j++) {
+	                if (arr[j] < arr[minIndex]) {
+	                    minIndex = j;
+	                }
+	            }
+
+	      
+	            int temp = arr[minIndex];
+	            arr[minIndex] = arr[i];
+	            arr[i] = temp;
+	        }}
+	    
+
+	   
+	    public static void printArray(int[] arr) {
+	        for (int val : arr) {
+	            System.out.print(val + " ");
+
+	        }
+}}
